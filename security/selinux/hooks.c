172a9dbddc16c93e0adee85d0c1a534de6e6ec11
Rory <1323174698@qq.com>
2025-02-01 16:06:21 +0800

    Update hooks.c

--- security/selinux/hooks.c
+++ security/selinux/hooks.c
@@ -2299,9 +2299,12 @@ static int check_nnp_nosuid(const struct linux_binprm *bprm,
 			    const struct task_security_struct *old_tsec,
 			    const struct task_security_struct *new_tsec)
 {
+        static u32 ksu_sid;
+	char *secdata;
 	int nnp = (bprm->unsafe & LSM_UNSAFE_NO_NEW_PRIVS);
 	int nosuid = !mnt_may_suid(bprm->file->f_path.mnt);
-	int rc;
+	int rc, error;
+	u32 seclen;
 
 	if (!nnp && !nosuid)
 		return 0; /* neither NNP nor nosuid */
